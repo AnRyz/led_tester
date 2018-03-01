39638bc640fd8d5f15e7021361fb0fd4dc394ce7
AnRyz <anna.ryzova@ucdconnect.ie>
2018-03-01 23:02:44 +0000

    function that calculates sum of leds is implemented and seems working

--- led_functions.py
+++ led_functions.py
@@ -1,2 +1,32 @@
 #this file contains led functions
 
+def SumLed (L,  x1, y1, x2, y2):
+    i  = 0
+    j  = 0
+    s  = 0
+    
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            s += L[i][j]
+    
+    print ("SumLed()", s)
+    return s
+   
+   
+   
+#main program   
+#declare 2D array in Python size w,h 
+w, h = 5, 3
+
+#Init with 0s
+L = [[10 for x in range(w)] for y in range(h)]    
+
+
+ledson = SumLed(L, 0,0, 5,3)   
+print ("Summ", ledson)    
+
+
+
+
+
+    
